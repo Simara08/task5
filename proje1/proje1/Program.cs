ff69fe801c92d4ca66ab243ff4ff78400315870d
Simara08 <7knywpc@code.edu.az>
2022-03-15 09:58:20 +0400

    created task5

---
+++ proje1/proje1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace proje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 28,38,12,22 };
+            int i = 0;
+            int caunt=0;
+            
+            
+            for ( i =0; i < array.Length; i++)
+            {
+                int sum = 0;
+
+                while (array[i] != 0)
+                {
+                    int r = array[i] % 10;
+                        sum = sum + r;
+                    array[i] = array[i] / 10; }
+                if (sum<10)
+                {
+                    caunt++;
+                }
+                
+
+                
+            }
+            
+            Console.WriteLine(caunt);
+
+        }
+    }
+}
