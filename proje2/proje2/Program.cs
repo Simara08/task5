aeee2225a88bf9b3ac1b183637c8793d7ca77adb
Simara08 <7knywpc@code.edu.az>
2022-03-15 09:59:01 +0400

    created proje2

---
+++ proje2/proje2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace proje2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int m = 50;
+            int i;
+            int caunt = 0;
+            for (i =n; i < m; i++)
+            {
+                if (i%15==0)
+                {
+                    caunt++;
+                }
+
+            }
+            Console.WriteLine(caunt);
+        }
+    }
+}
